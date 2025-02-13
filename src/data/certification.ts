fe6130b3b92c76dfc8044e973bbcd3d7778f44aa
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2025-02-13 10:07:05 +0300

    Modification du chemin des images

--- src/data/certification.ts
+++ src/data/certification.ts
@@ -5,42 +5,42 @@ const dataCertification: Certification[] = [
       title: "C++ Certificate",
       issuer: "Sololearn",
       date: "Sep 2024",
-      image: "/public/assets/images/CPlusPlus_certificate.jpg",
+      image: "/public/images/CPlusPlus_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "C++ Certificate",
       issuer: "Coding Game",
       date: "Sep 2024",
-      image: "/public/assets/images/Certification-1_page-0001.jpg",
+      image: "/public/images/Certification-1_page-0001.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "CSS Certificate",
       issuer: "Sololearn",
       date: "Sep 2024",
-      image: "/public/assets/images/CSS_certificate.jpg",
+      image: "/public/images/CSS_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "C Course Certificate",
       issuer: "Sololearn",
       date: "",
-      image: "/public/assets/images/C_certificate.jpg",
+      image: "/public/images/C_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "PHP Certificate",
       issuer: "Sololearn",
       date: "",
-      image: "/public/assets/images/PHP_certificate.jpg",
+      image: "/public/images/PHP_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "HTML Certificate",
       issuer: "Sololearn",
       date: "",
-      image: "/public/assets/images/HTML_certificate.jpg",
+      image: "/public/images/HTML_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
 ];

1a0ce4c206a88d46e4e5a3705d54bf0e1baa919b
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2025-02-13 10:05:37 +0300

    Modification du chemin des images

--- src/data/certification.ts
+++ src/data/certification.ts
@@ -5,42 +5,42 @@ const dataCertification: Certification[] = [
       title: "C++ Certificate",
       issuer: "Sololearn",
       date: "Sep 2024",
-      image: "/src/assets/images/CPlusPlus_certificate.jpg",
+      image: "/public/assets/images/CPlusPlus_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "C++ Certificate",
       issuer: "Coding Game",
       date: "Sep 2024",
-      image: "/src/assets/images/Certification-1_page-0001.jpg",
+      image: "/public/assets/images/Certification-1_page-0001.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "CSS Certificate",
       issuer: "Sololearn",
       date: "Sep 2024",
-      image: "/src/assets/images/CSS_certificate.jpg",
+      image: "/public/assets/images/CSS_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "C Course Certificate",
       issuer: "Sololearn",
       date: "",
-      image: "/src/assets/images/C_certificate.jpg",
+      image: "/public/assets/images/C_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "PHP Certificate",
       issuer: "Sololearn",
       date: "",
-      image: "/src/assets/images/PHP_certificate.jpg",
+      image: "/public/assets/images/PHP_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
       title: "HTML Certificate",
       issuer: "Sololearn",
       date: "",
-      image: "/src/assets/images/HTML_certificate.jpg",
+      image: "/public/assets/images/HTML_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
 ];

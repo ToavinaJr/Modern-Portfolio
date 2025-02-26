f5bee00366d7925b75ddb0f4fee8a52285e8b56d
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2025-02-26 14:33:42 +0300

    Renverser les networks bottons et les textes en version mobile

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,7 +2,7 @@ import { Github, Linkedin, Download } from 'lucide-react';
 import { FaHandshake } from "react-icons/fa";
 
 export const HeroSection = () => (
-  <section id="hero" className="container mx-auto px-6 py-20 flex flex-col md:flex-row items-center gap-12 bg-gradient-to-b from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 rounded-lg shadow-xl animate-fade-in">
+  <section id="hero" className="container mx-auto px-6 py-8 md:py-20 flex flex-col md:flex-row items-center gap-12 bg-gradient-to-b from-gray-100 to-white dark:from-gray-900 dark:to-gray-800 rounded-lg shadow-xl animate-fade-in">
     <div className="md:w-1/3 flex justify-center">
       <img
         src="/images/photo_profil.jpg"
@@ -10,14 +10,14 @@ export const HeroSection = () => (
         className="rounded-full w-48 h-48 md:w-56 md:h-56 object-cover shadow-lg border-4 border-white dark:border-gray-700 transition-all duration-300 hover:scale-105 animate-slide-in-left"
       />
     </div>
-    <div className="md:w-2/3 text-center md:text-left space-y-6 animate-slide-in-right">
-      <h2 className="sm:text-3xl md:text-5xl font-extrabold text-gray-900 dark:text-white animate-text-focus-in">
+    <div className="md:w-2/3 text-center md:text-left space-y-3  md:space-y-6 animate-slide-in-right">
+      <h2 className="text-3xl md:text-5xl font-extrabold text-gray-900 dark:text-white animate-text-focus-in">
         Junior Developer
       </h2>
       <p className="text-lg text-gray-700 dark:text-gray-300 max-w-lg mx-auto md:mx-0 animate-fade-in-up">
         Passionate about creating innovative web solutions with modern technologies.
       </p>
-      <div className="flex flex-col md:flex-row gap-6 justify-center md:justify-start mt-4 items-center animate-fade-in-up">
+      <div className="flex flex-col-reverse md:flex-row gap-6 justify-center md:justify-start mt-4 items-center animate-fade-in-up">
         <div className="flex gap-6">
           <a 
             href="https://github.com/ToavinaJr" 

a510c31dcaf428460cb007f0c396aaf0f8f7dd0c
Toavina Sylvianno <toavinasylvianno14@gmail.com>
2025-02-28 01:22:51 +0300

    Rearrengement des ordres des certificats

--- src/data/certification.ts
+++ src/data/certification.ts
@@ -2,17 +2,38 @@ import { Certification } from "../types";
 
 const dataCertification: Certification[] = [
     {
-      title: "C++ Certificate",
-      issuer: "Sololearn",
-      date: "Sep 2024",
-      image: "/images/CPlusPlus_certificate.jpg",
-      certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
+      title: "Problem Solving Certificate (Basic)",
+      issuer: "Hackerrank",
+      date: "",
+      image: "/images/problem_solving_basic certificate.jpg",
+      certificateLink: "https://www.hackerrank.com/certificates/795471e261c0"
+    },
+    {
+      title: "Problem Solving Certificate (Intermediate)",
+      issuer: "Hackerrank",
+      date: "",
+      image: "/images/problem_solving_intermediate certificate.jpg",
+      certificateLink: "https://www.hackerrank.com/certificates/8981dda9acde"
+    },
+    {
+      title: "SQL Certificate (Basic)",
+      issuer: "Hackerrank",
+      date: "",
+      image: "/images/sql_basic certificate.jpg",
+      certificateLink: "https://www.hackerrank.com/certificates/910e8ae489b3"
     },
     {
       title: "C++ Certificate",
       issuer: "Coding Game",
       date: "Sep 2024",
       image: "/images/Certification-1_page-0001.jpg",
+      certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
+    },
+    {
+      title: "C++ Certificate",
+      issuer: "Sololearn",
+      date: "Sep 2024",
+      image: "/images/CPlusPlus_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
     },
     {
@@ -42,27 +63,6 @@ const dataCertification: Certification[] = [
       date: "",
       image: "/images/HTML_certificate.jpg",
       certificateLink: "https://www.codingame.com/certification/NHIGu5MqBVjHZfgKs8iH1g"
-    },
-    {
-      title: "Problem Solving Certificate (Basic)",
-      issuer: "Hackerrank",
-      date: "",
-      image: "/images/problem_solving_basic certificate.jpg",
-      certificateLink: "https://www.hackerrank.com/certificates/795471e261c0"
-    },
-    {
-      title: "Problem Solving Certificate (Intermediate)",
-      issuer: "Hackerrank",
-      date: "",
-      image: "/images/problem_solving_intermediate certificate.jpg",
-      certificateLink: "https://www.hackerrank.com/certificates/8981dda9acde"
-    },
-    {
-      title: "SQL Certificate (Basic)",
-      issuer: "Hackerrank",
-      date: "",
-      image: "/images/sql_basic certificate.jpg",
-      certificateLink: "https://www.hackerrank.com/certificates/910e8ae489b3"
     },
 ];
 
